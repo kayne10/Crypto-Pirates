1c6661e99c9ba6b4ac4ce2292a771225a99fb700
Troy Kayne <troy@Troys-MacBook-Pro.local>
2017-11-12 20:11:24 -0700

    Attempted Scatterplot. Need to figure out milestone calculations

--- pandas_scripts/bitcoinAnalytics.py
+++ pandas_scripts/bitcoinAnalytics.py
@@ -1,7 +1,10 @@
 # this will analyze bitcoin data
 # this will contain the pandas code
-import pandas as py
+import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-# load csv file as data frame
-df = pd.read_csv('Datasets/bitcoin_dataset.csv')
+data = pd.read_csv('/Users/troy/Github/courses/crypto-pirates/Datasets/bitcoin_price.csv')
+
+plt.scatter(data['Date'], data['Volume'])
+# plt.savefig(sample.pdf)
