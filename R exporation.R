2ffb0787c3c66327bf07d3f6be43ac98ffb0a7f1
schubydooo <scott.schubert@colorado.edu>
2017-11-30 10:00:05 -0700

    test
    Test commit

--- R exporation.R
+++ R exporation.R
@@ -27,8 +27,13 @@ plot(bitcoin)
 #Linear model
 
 #
+#Initial trial
 bitcoin.lmod <- lm(bitcoin$Close ~ bitcoin$Open)
 summary(bitcoin.lmod)
 plot(bitcoin$Open, bitcoin$Close, xlab = "Open", ylab = "Close", main = "Open vs Close")
 abline(bitcoin.lmod, col = "red")
+#Unsurprisingly, there is strong correlation between open and close with 99.6% of variation explained
+#The dots further from our linear abline would be interesting to explore as they denote lines such that 
+#the stock value of bitcoin changed beyond the norm
+
 
